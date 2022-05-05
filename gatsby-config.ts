924156207e7615295bf57a43406af9481ea984dd
Birjot Bala <61438495+Birjot-Bala@users.noreply.github.com>
2022-05-05 12:17:08 -0400

    gatsby plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,10 +2,19 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Birjot-Bala`,
+    title: `Birjot Bala`,
     siteUrl: `https://birjot-bala.github.io`,
   },
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `text`,
+        path: `${__dirname}/src/text/`,
+      },
+    },
+  ],
 }
 
 export default config
